3fd6ab865eeaf45a8b66a0157abb688ac5f49a3c
Andrey Strogush <magistrogush@gmail.com>
2019-03-04 12:12:40 +0200

    +LocalDate in main method/test

--- src/carExample/MainTest.java
+++ src/carExample/MainTest.java
@@ -1,11 +1,14 @@
 package carExample;
 
+import java.time.LocalDate;
+
 public class MainTest {
     public static void main(String[] args) {
-        Car myCar = new Car("2015", "Diesel", 180, 6.2d, 7, 2, 0);
+        Car myCar = new Car(LocalDate.now(),"Diesel", 180, 6.2d, 7, 2, 0);
         myCar.setTheDefoltSetOfDoors();
         myCar.setTheDefoltSetOfWheels();
 
             CarRunner.runCar(myCar);
     }
+
 }
